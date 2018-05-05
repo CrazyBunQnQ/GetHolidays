baf0bfc7965cedb52752640efbf2ca76188de891
CrazyBunQnQ <baobao222222@qq.com>
2018-05-05 14:27:20 +0800

    优化获取一年假日函数的代码

--- holidays.py
+++ holidays.py
@@ -10,12 +10,19 @@ rexBody = r'(<tbody>.*</tbody>)'
 rexTable = r'(colspan="7">[A-Z]{3} \((\d{1,2}).{100,1600}</table>)'
 rexTd = r'(<td class="td\-([akrsuntophl\-]{1,10})">((\&ensp;)|(\d{1,6}))</td>)'
 rexYear = r'(\d{4})'
+pageUrl = ""
 
 
-def getAllHolidays(year=time.strftime("%Y"), lan="zh_cn"):
-# def getAllHolidays(year=time.strftime("%Y%m%d"), lan="zh_cn"):
+def getYearHolidays(year=time.strftime("%Y"), lan="zh_cn"):
+# def getMonthHolidays(year=time.strftime("%Y%m"), lan="zh_cn"):
+# def getDayHolidays(year=time.strftime("%Y%m"), lan="zh_cn"):
     data = {}
-    pageUrl = "http://holidays-calendar.net/calendar_%s/china_%s.html" % (language, language)
+    if int(time.strftime(("%Y"))) - int(year) > 2 or int(time.strftime(("%Y"))) - int(year) < 2 :
+        data['error'] = "只等查 %s、%s 和 %s 年的节假日" % (time.strftime("%Y"), str(int(time.strftime("%Y"))-1), str(int(time.strftime("%Y"))-2))
+        return json.dumps(data)
+    urlYear = "%s/" % year if time.strftime("%Y") != year else ""
+    pageUrl =  "http://holidays-calendar.net/%scalendar_%s/china_%s.html" % (urlYear,language, language)
+    print(pageUrl)
     try:
         Response = urllib.request.urlopen(pageUrl, timeout=10)
         Html = Response.read()
@@ -25,9 +32,6 @@ def getAllHolidays(year=time.strftime("%Y"), lan="zh_cn"):
         Html = Html.decode('utf-8')
         tableList = re.findall(rexTable, Html)
         for tableHtml in tableList:
-            # print("table 长度：%s" % str(len(tableHtml)))
-            # print(tableHtml[0])
-            # print("%s 月" % tableHtml[1])
             tdList = re.findall(rexTd, tableHtml[0])
             months = {}
             month = "%s%s" % (year, tableHtml[1].zfill(2))
@@ -38,8 +42,6 @@ def getAllHolidays(year=time.strftime("%Y"), lan="zh_cn"):
                         dayType = 0
                     if "suntop-hol" == tdHtml[1]:
                         dayType = 2
-                    # print("%s 月 %s 日是 %s 日" % (tableHtml[1], tdHtml[2], tdHtml[1]))
-                    # print("%s 月 %s 日是 %s 日" % (tableHtml[1], tdHtml[2], dayType))
                     day = "%s%s%s" % (year, tableHtml[1].zfill(2), tdHtml[2].zfill(2))
                     months[day] = dayType
             data[month] = months
@@ -47,4 +49,8 @@ def getAllHolidays(year=time.strftime("%Y"), lan="zh_cn"):
     return json.dumps(data)
 
 
-print(getAllHolidays())
+print(getYearHolidays("2019"))
+print(getYearHolidays())
+print(getYearHolidays("2017"))
+print(getYearHolidays("2016"))
+print(getYearHolidays("2015"))
